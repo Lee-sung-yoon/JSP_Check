f8c3ca4fbff75cc15483c9c3cebd957ad86b9eae
LEE sy <tjddbs1412@naver.com>
2023-05-15 16:55:29 +0900

    JSP 기본 세팅 및 출력 확인

---
+++ src/main/java/com/example/jsp_check/GugudanServlet.java
@@ -0,0 +1,28 @@
+package com.example.jsp_check;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 utf-8로 해석
+        resp.setCharacterEncoding("UTF-8"); // 완성되는 html의 인코딩을 utf-8로 하겠다.
+        resp.setContentType("text/html; charset-utf-8"); // 브라우저에게 우리가 만든 결과물이 utf-8이라고 알리는 의미
+
+        int dan = Integer.parseInt(req.getParameter("dan")); // 문장으로 인식되는 변수를 숫자로 변형
+        int limit = Integer.parseInt(req.getParameter("limit"));;
+
+        resp.getWriter().append(String.format("<h1>%d단</h1>\n", dan));
+
+        for (int i = 1; i <= limit; i++) {
+            resp.getWriter().append(String.format("<div>%d * %d = %d </div>\n", dan, i, dan * i));
+        }
+    }
+}
---
+++ src/main/java/com/example/jsp_check/HelloServlet1.java
@@ -0,0 +1,21 @@
+package com.example.jsp_check;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 utf-8로 해석
+        resp.setCharacterEncoding("UTF-8"); // 완성되는 html의 인코딩을 utf-8로 하겠다.
+        resp.setContentType("text/html; charset-utf-8"); // 브라우저에게 우리가 만든 결과물이 utf-8이라고 알리는 의미
+
+        resp.getWriter().append("HI!!!!");
+    }
+}
